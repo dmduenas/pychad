dc157a60ae474687349fd5f42b850acae60ae9db
diego <153370016+dmduenas@users.noreply.github.com>
2025-10-17 10:43:58 +0800

    helloo

--- game/game.py
+++ game/game.py
@@ -1,13 +1,40 @@
 class User():
-    def __init__(self, username, email, password):
-        self.username = username
-        self.email = email
-        self.password = password
+    def __init__(self, id_number, fname, lname, age, marital_status, region, job):
+        self.id_number = id_number
+        self.fname = fname
+        self.lname = lname
+        self.age = age
+        self.marital_status = marital_status
+        self.region = region
+        self.job = job
 
-    def say_hi_to_user(self, user):
-        print(f"Sending message to {user.username}: Hi {user.username}, it's {self.username}") 
+    def set_id_number(self, new_id_number):
+        self.id_number = new_id_number
 
-user1 = User("Bruce", "bat@gmail.com", "abc")
-user2 = User("Clarke", "superman@outlook.com", "123")
+    def set_fname(self, new_fname):
+        self.fname = new_fname
+    
+    def set_lname(self, new_lname):
+        self.lname = new_lname
 
-user1.say_hi_to_user(user2)
+    def set_age(self, new_age):
+        self.age = new_age
+
+    def set_marital_status(self, new_marital_status):
+        self.marital_status = new_marital_status
+
+    def set_region(self, new_region):
+        self.region = new_region
+
+    def set_job(self, new_job):
+        self.job = new_job
+
+new_id_number = input("Enter ID Number: ")
+new_fname = input("Enter First Name: ")
+new_lname = input("Enter Last Name: ")
+new_age = int(input("Enter Age: "))
+new_marital_status = input("Enter Marital Status: ")
+new_region = input("Enter Region: ")
+new_job = input("enter Job Description: ")
+
+user = User(set_id_number(), set_fname(), set_lname(), set_age(), set_marital_status(), set_region(), set_job())
--- script.py
+++ script.py
@@ -1,13 +1,40 @@
-class Person:
-    def __init__(self, name, age):
-        self.name = name
+class User():
+    def __init__(self, id_number, fname, lname, age, marital_status, region, job):
+        self.id_number = id_number
+        self.fname = fname
+        self.lname = lname
         self.age = age
+        self.marital_status = marital_status
+        self.region = region
+        self.job = job
 
-    def greet(self):
-        print(f"Hello my name is {self.name} and I am {self.age} years old")
+    def set_id_number(self, new_id_number):
+        self.id_number = new_id_number
 
-person1 = Person("Diego", 34)
+    def set_fname(self, new_fname):
+        self.fname = new_fname
+    
+    def set_lname(self, new_lname):
+        self.lname = new_lname
 
-person1.name = "Aswang"
+    def set_age(self, new_age):
+        self.age = new_age
 
-person1.greet()
+    def set_marital_status(self, new_marital_status):
+        self.marital_status = new_marital_status
+
+    def set_region(self, new_region):
+        self.region = new_region
+
+    def set_job(self, new_job):
+        self.job = new_job
+
+new_id_number = input("Enter ID Number: ")
+new_fname = input("Enter First Name: ")
+new_lname = input("Enter Last Name: ")
+new_age = int(input("Enter Age: "))
+new_marital_status = input("Enter Marital Status: ")
+new_region = input("Enter Region: ")
+new_job = input("Enter Job Description: ")
+
+user = User(new_id_number, new_fname, new_lname, new_age, new_marital_status, new_region, new_job)
