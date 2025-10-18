4d214f3ebf7c575d7a0bbefdc9d3dd3f0bd76222
diego <153370016+dmduenas@users.noreply.github.com>
2025-10-18 11:43:41 +0800

    super

--- data.py
+++ data.py
@@ -1,3 +1,9 @@
-class CoffeeMachine:
-    water = 500
-    
+class BankAccount:
+    MIN_BALANCE = 100
+
+    def __init__(self, owner, balance = 0):
+        self.owner = owner
+        self.balance = balance
+
+
+
